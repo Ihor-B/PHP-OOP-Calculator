9e2387c4cd718c06d71fb277c02fde8c31c9e2c7
unknown <boltyk@gmail.com>
2014-12-08 19:19:04 -0800

    version updated

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <section id="calculator">
         <div class="container">
             <div class="row">
-            	<h2>Simple Calculator v0.1</h2>
+            	<h2>Simple Calculator v0.2</h2>
                 <div class="col-12">
 					<form id="calc" accept-charset="utf-8">
 						<label class="input_label" for="operator">Operator:</label>
